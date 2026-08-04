79323b9e7c4b3a7d4cfb519b32888365b595db24
umamaheshwari119 <savithramma441@gmail.com>
2026-08-04 13:13:10 +0530

    Add files via upload

---
+++ string 1.py
@@ -0,0 +1,13 @@
+ str1="welcome to andhra "
+ print(str1)
+ print(type(str1))
+ #str1[0]="Hello"
+ print(str1)
+ 
+print(str1.spilt('-'))
+#print(str2)
+#print(type(str1))
+
+print(str1.upper())
+print(str1.lower())
+
