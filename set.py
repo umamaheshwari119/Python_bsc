4b75209fb26c152e5b7ee8b180f3d56f3a01f2c3
umamaheshwari119 <savithramma441@gmail.com>
2026-08-04 11:59:39 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,22 @@
+#s1=(1,1,2,2,4,4)
+#s2=(1,4,35,1)
+#print(s1)
+#print(type(s1))
+#s1.add("uma")
+#print(s1)
+#s1.remove(1)
+#print(s1)
+#print(s1.difference)
+
+#l1=[1,1,2,2,3,4,5,5,6]
+#print[l1]
+#print(type(l1))
+#l2=list(set(l1))
+#print(12)
+#print(type(l2))
+
+a=input("enter the string")
+b=set(a)
+c=('a','e','i','o','u')
+d=b.intersection(c)
+print("vowels present in given string are" ,d)
