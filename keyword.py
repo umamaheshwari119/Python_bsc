32a6c847dd6429bd3843d20c50dff5e114563197
umamaheshwari119 <savithramma441@gmail.com>
2026-08-04 13:05:01 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,22 @@
+#Rule 3
+aaaa=21
+print(aaaaa)
+print()
+
+#Rule 4
+A=12
+a=20
+print(id(A),id(a))
+
+# case sensitive
+C=10
+b=10
+print(id(c),id(b))
+
+#Rule 5
+_a=12
+print(_a)
+
+#Rule 6
+import keyword as s1
+print(s1,kwlist)
