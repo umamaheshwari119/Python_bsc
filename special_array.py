bd19f0ee19b6bc4292f944fe6c80ed7a23778123
umamaheshwari119 <savithramma441@gmail.com>
2026-08-13 13:12:35 +0530

    Add files via upload

---
+++ special_array.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+zeros = np.zeros(5)
+ones = np.ones(5)
+numbers = np.arange(1, 11)
+values = np.linspace(0, 10, 6)
+
+print("Zeros:")
+print(zeros)
+
+print("\nOnes:")
+print(ones)
+
+print("\nNumbers from 1 to 10:")
+print(numbers)
+
+print("\nSix equally spaced values from 0 to 10:")
+print(values)
